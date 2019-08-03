7fc5aaa09b02ffcbec62dd4af4ffff46256e43d7
Lucas H. Xu <xuhang57@users.noreply.github.com>
2019-08-03 14:49:22 -0400

    pep8 fix

--- Set1-Basics/fixed-xor/fixedXor.py
+++ Set1-Basics/fixed-xor/fixedXor.py
@@ -10,15 +10,16 @@ from Crypto.Util.strxor import strxor
 
 def fixed_xor(num1, num2):
     """
-	    Convert two equal-length buffers to XOR combination
+        Convert two equal-length buffers to XOR combination
 
-		: param num1: a hex number in string format
-		: param num2: a hex number in string format
-		: return: a hex number in string format
-	"""
+        : param num1: a hex number in string format
+        : param num2: a hex number in string format
+        : return: a hex number in string format
+        """
     return hexlify(strxor(unhexlify(num1), unhexlify(num2))).decode()
 
+
 if __name__ == "__main__":
-	input_str1 = str(input("Give a hex number: "))
-	input_str2 = str(input("Give another hex number: "))
-	print(fixed_xor(input_str1, input_str2))
+    input_str1 = str(input("Give a hex number: "))
+    input_str2 = str(input("Give another hex number: "))
+    print(fixed_xor(input_str1, input_str2))
